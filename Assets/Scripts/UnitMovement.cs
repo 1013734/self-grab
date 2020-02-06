13a7aa7c81a1a3a3e49f16c9bd8a6d36d2475474
Niall <1013734@student.sae.edu.au>
2020-02-06 14:56:45 +1100

    INIT

---
+++ Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public void Start ()
+    { 
+        //rb = GetComponent< Rigidbody>();
+    }
+
+}
