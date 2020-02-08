5acc28cefd55264bcce2a6c83529c3fb82b8f7ec
Niall <1013734@student.sae.edu.au>
2020-02-08 18:28:37 +1100

    0208 Update

---
+++ Assets/Scripts/DuoUnitSearch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuoUnitSearch : MonoBehaviour {
+
+	
+	// need state machine, moving, searching, attacking, dead, enraged.
+	// if partner is not within radius && not in combat, begin searching for them.
+	// if partner is inside radius && in combat, attack same enemy.
+	// if partner dies while inside radius, state enraged (boosted stats).
+	// if partner dies outside of radius, ignore partner details and continues as solo unit with moving, attack, dead states.
+
+}
